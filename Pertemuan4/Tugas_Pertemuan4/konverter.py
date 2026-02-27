f9f38dbdf9a425c6228d469cf699623b02f3de27
ilyassrahmann <ilyassrahmann@gmail.com>
2026-02-27 18:59:09 +0700

    update converter.py

--- Pertemuan4/Tugas_Pertemuan4/konverter.py
+++ Pertemuan4/Tugas_Pertemuan4/konverter.py
@@ -1,15 +1,15 @@
 #Buat file konverter.py berisi fungsi konversi (IDR ke mata uang lain dan sebaliknya).
 from kurs import kurs
 
-def idr_to_currency(amount_idr, currency):
+def idr_to_currency(jumlah_idr, currency):
     if currency in kurs:
-        return amount_idr / kurs[currency]
+        return jumlah_idr / kurs[currency]
     else:
         raise ValueError("Mata uang tidak ditemukan dalam kurs.")
 
-def currency_to_idr(amount_currency, currency):
+def currency_to_idr(jumlah_currency, currency):
     if currency in kurs:
-        return amount_currency * kurs[currency]
+        return jumlah_currency * kurs[currency]
     else:
         raise ValueError("Mata uang tidak ditemukan dalam kurs.")
     
